e0cb6e8b553f73ac922c385d626e0e9a99c5ad7b
Trevor Johannessen <trevor.johannessen@gmail.com>
2025-05-04 12:04:21 -0400

    Added Controller to server

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from voice import Voice
+from controller import Controller
+
 class Prompt(BaseModel):
     return_type: str
     prompt: str
@@ -13,9 +15,10 @@ voice = Voice(
     model="0yy6aROli0UmPMyExQ1S",
     directory="/mnt/fs1/media/audio/voice-lines/monika/history"
 )
+controller = Controller()
 
 @app.post("/prompt")
-async def read_root(data: Prompt):
+async def prompt(data: Prompt):
     # Send prompt to orchestrator
-    
-    return {"message": f"Content: {data.return_type}, prompt: {data.prompt}"}
+    response = await controller.prompt(data.prompt)
+    return {"message": response}
