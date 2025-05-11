95ffb84bc56d985d5f0311f01edd2012af96ad75
Trevor Johannessen <trevor.johannessen@gmail.com>
2025-05-11 18:25:25 -0400

    Changed model of orchestration agent to o4-mini

--- orchestrationAgent.py
+++ orchestrationAgent.py
@@ -1,10 +1,11 @@
-from agents import Agent
+from agents import Agent, ModelSettings
 
 class OrchestrationAgent(Agent):
     def __init__(self, agents=[], outputType=None):
         super().__init__(
             name="orchestration_agent",
-            instructions="Hand off to the apropriate agent based on the request, if it seems like there is nothing more to be done, call the conversation agent. You will also be given a JSON including the current chat history. This is for context only, do not consider it when handing off.",
-            handoffs=agents
+            instructions="Hand off to the apropriate agent based on the request, if it seems like there is nothing more to be done, call the conversation agent. Consider the entire prompt when thinking about what agent to handoff to.",
+            handoffs=agents,
+            model="gpt-4o-mini",
         )
  
